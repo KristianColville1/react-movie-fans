d243ccdd0020f6100b69e9274f541475f21bd836
Kristian Colville <kristiancolville1@gmail.com>
2026-08-09 15:12:06 +0100

    Extract App component out of entry point into its own file

--- src/index.tsx
+++ src/index.tsx
@@ -1,63 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
-import HomePage from "@pages/homePage";
-import MoviePage from "@pages/movieDetailsPage";
-import FavouriteMoviesPage from "@pages/favouriteMoviesPage"; // NEW
-import MovieReviewPage from "@pages/movieReviewPage";
-import SiteHeader from "@organisms/siteHeader";
-import UpcomingMoviesPage from "@pages/upcomingMoviesPage"; // NEW
-import { QueryClientProvider, QueryClient } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
-import MoviesContextProvider from "@contexts/moviesContext";
-import AddMovieReviewPage from "@pages/addMovieReviewPage";
-
-
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 360000,
-            refetchInterval: 360000,
-            refetchOnWindowFocus: false,
-        },
-    },
-});
-
-
-const App = () => {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <SiteHeader />
-                <MoviesContextProvider>
-                    <Routes>
-                        <Route
-                            path="/movies/favourites"
-                            element={<FavouriteMoviesPage />}
-                        />
-                        <Route
-                            path="/movies/upcoming"
-                            element={<UpcomingMoviesPage />}
-                        />
-                        <Route path="/movies/:id" element={<MoviePage />} />
-                        <Route
-                            path="/reviews/form"
-                            element={<AddMovieReviewPage />}
-                        />
-                        <Route
-                            path="/reviews/:id"
-                            element={<MovieReviewPage />}
-                        />
-
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="*" element={<Navigate to="/" />} />
-                    </Routes>
-                </MoviesContextProvider>
-            </BrowserRouter>
-            <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-    );
-};
+import App from "@/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
