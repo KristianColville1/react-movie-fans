65f0bb1fe58fa28462530133e1206e96506e1fb8
Kristian Colville <kristiancolville1@gmail.com>
2026-08-10 08:24:32 +0100

    Add a story for the movie filter UI

---
+++ src/components/organisms/movieFilterUI/movieFilterUI.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import MovieFilterUI from "@organisms/movieFilterUI";
+import { action } from "@storybook/addon-actions";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 360000,
+            refetchInterval: 360000,
+            refetchOnWindowFocus: false,
+        },
+    },
+});
+
+const meta = {
+    title: "Organisms/MovieFilterUI",
+    component: MovieFilterUI,
+    decorators: [
+        (Story) => (
+            <QueryClientProvider client={queryClient}>
+                <Story />
+            </QueryClientProvider>
+        ),
+    ],
+} satisfies Meta<typeof MovieFilterUI>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+    args: {
+        onFilterValuesChange: action("filter input"),
+        titleFilter: "",
+        genreFilter: "0",
+    },
+};
+Basic.storyName = "Default";
