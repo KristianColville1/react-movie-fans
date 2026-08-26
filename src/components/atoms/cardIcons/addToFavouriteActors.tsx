e348e750479480daf1c625399ce9780cadcc647e
Kristian Colville <kristiancolville1@gmail.com>
2026-08-26 21:51:23 +0100

    Toggle the favourite actor icon and show the current state

--- src/components/atoms/cardIcons/addToFavouriteActors.tsx
+++ src/components/atoms/cardIcons/addToFavouriteActors.tsx
@@ -2,28 +2,46 @@ import React, { MouseEvent, useContext } from "react";
 import { MoviesContext } from "@contexts/moviesContext";
 import IconButton from "@mui/material/IconButton";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { BaseActorProps } from "@typings/interfaces";
 
 /**
- * Icon button that adds an actor to the user's favourites.
+ * Icon button that adds an actor to the user's favourites, or takes them out
+ * again. Filled means the actor is already a favourite.
  *
- * @param actor The actor to add.
+ * @param actor The actor to add or remove.
  * @returns JSX.Element
  */
 const AddToFavouriteActorsIcon: React.FC<BaseActorProps> = (actor) => {
-    const context = useContext(MoviesContext);
+    const { favouriteActors, addToFavouriteActors, removeFromFavouriteActors } =
+        useContext(MoviesContext);
+
+    const isFavourite = favouriteActors.includes(actor.id);
 
     const onUserSelect = (e: MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
-        context.addToFavouriteActors(actor.id);
+        if (isFavourite) {
+            removeFromFavouriteActors(actor.id);
+            return;
+        }
+        addToFavouriteActors(actor.id);
     };
+
     return (
         <IconButton
-            aria-label="add actor to favorites"
+            aria-label={
+                isFavourite
+                    ? "remove actor from favorites"
+                    : "add actor to favorites"
+            }
             color="inherit"
             onClick={onUserSelect}
         >
-            <FavoriteIcon color="inherit" fontSize="large" />
+            {isFavourite ? (
+                <FavoriteIcon color="inherit" fontSize="large" />
+            ) : (
+                <FavoriteBorderIcon color="inherit" fontSize="large" />
+            )}
         </IconButton>
     );
 };
