8a7cd55df950dd66fc394d652e105b8387d32311
Kristian Colville <kristiancolville1@gmail.com>
2026-08-27 22:15:22 +0100

    Style the reviews table and the full review

--- src/components/molecules/movieReview/index.tsx
+++ src/components/molecules/movieReview/index.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
 import { Review } from "@typings/interfaces";
 
 /**
@@ -9,10 +12,25 @@ import { Review } from "@typings/interfaces";
  */
 const MovieReview: React.FC<Review> = (props) => {
     return (
-        <>
-            <p>Review By: {props.author} </p>
-            <p>{props.content} </p>
-        </>
+        <Paper className="rounded-xl bg-surface p-6 text-navajo-white">
+            <div className="mb-4 flex items-center gap-3">
+                <Avatar className="bg-ocean-mist font-semibold text-jet-black">
+                    {props.author?.charAt(0).toUpperCase()}
+                </Avatar>
+                <Typography variant="h6" component="h3">
+                    {props.author}
+                </Typography>
+            </div>
+
+            <Typography
+                variant="body1"
+                component="p"
+                className="leading-relaxed whitespace-pre-line text-navajo-white/80"
+            >
+                {props.content}
+            </Typography>
+        </Paper>
     );
 };
+
 export default MovieReview;
